6090d6ee0881b93cd18c75911eacb4a095ab7b53
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-16 00:12:41 +0530

    c11

---
+++ cmake-build-debug/CMakeFiles/c11_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c11_tutorial.dir/main.c.obj"
+  "c11_tutorial.exe"
+  "c11_tutorial.exe.manifest"
+  "c11_tutorial.lib"
+  "c11_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c11_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
